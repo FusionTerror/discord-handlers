9693956d5314e6adaa22ca29bea0136ca94539f2
Fusion Terror <64173400+FusionTerror@users.noreply.github.com>
2021-06-24 15:52:07 -0400

    Another Commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 module.exports = {
     events: async (eventFiles, path, client) => {
         for (const file of eventFiles) {
-            const event = require(`${path}/${file}`);
+            const event = require(`../../${path}/${file}`);
             console.log(`Event: ${event.name} | Loaded Successfully.`);
             if (event.once) client.once(event.name, (...args) => event.execute(...args, client));
             else client.on(event.name, (...args) => event.execute(...args, client));
@@ -16,7 +16,7 @@ module.exports = {
         for (const folder of commandFolders) {
             const commandFiles = fs.readdirSync(`${path}/${folder}`).filter(file => file.endsWith('.js'));
             for (const file of commandFiles) {
-                const command = require(`${path}/${folder}/${file}`);
+                const command = require(`../../${path}/${folder}/${file}`);
                 client.commands.set(command.name, command);
                 console.log(`Command: ${command.name} | Loaded Successfully.`);
             }
