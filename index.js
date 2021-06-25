f9e09e7f11103e1a1af256c01787b373fe076ac7
Fusion Terror <64173400+FusionTerror@users.noreply.github.com>
2021-06-25 00:17:11 -0400

    1.2.2 Commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports = {
     djsEvents: async (eventFiles, path, client) => {
         for (const file of eventFiles) {
             const event = require(`../../${path}/${file}`);
-            console.log(`Event: ${event.name} | Loaded Successfully.`);
+            console.log(`Discord.JS Event: ${event.name} | Loaded Successfully.`);
             if (event.once) client.once(event.name, (...args) => event.execute(...args, client));
             else client.on(event.name, (...args) => event.execute(...args, client));
         };
@@ -14,6 +14,7 @@ module.exports = {
     mongoEvents: async (eventFiles, path, client, mongoose) => {
         for (const file of eventFiles) {
             const event = require(`../../${path}/${file}`);
+            console.log(`Mongoose Connection Event: ${event.name} | Loaded Successfully.`);
             if (event.once) mongoose.connection.once(event.name, (...args) => event.execute(...args, client));
             else mongoose.connection.on(event.name, (...args) => event.execute(...args, client));
         }
